acb3430befb528d3c639cd328d6bc52724e622a5
rakeshrpandey012-droid <rakeshrpandey012@gmail.com>
2026-04-30 12:47:09 +0530

    Add files via upload

---
+++ Components/ProductDisplay/ProductDisplay.jsx
@@ -0,0 +1,58 @@
+import React, { useContext } from 'react'
+import './ProductDisplay.css'
+import star_icon from "../Assets/star_icon.png";
+import star_dull_icon from "../Assets/star_dull_icon.png";
+import { ShopContext } from '../../Context/ShopContext';
+
+const ProductDisplay = (props) => {
+    const {product} = props;
+    const {addToCart} = useContext(ShopContext);
+  return (
+    <div className='productdisplay'>
+        <div className="productdisplay-left">
+            <div className="productdisplay-img-list">
+                <img src={product&&product.image} alt="" />
+                <img src={product&&product.image} alt="" />
+                <img src={product&&product.image} alt="" />
+                <img src={product&&product.image} alt="" />
+            </div>
+            <div className="productdisplay-img">
+                <img className="productdisplay-main-img" src={product&&product.image} alt="" />
+            </div>
+        </div>
+        <div className="productdisplay-right">
+            <h1>{product?.name}</h1>
+            <div className="productdisplay-right-star">
+                <img src={star_icon} alt="" />
+                <img src={star_icon} alt="" />
+                <img src={star_icon} alt="" />
+                <img src={star_icon} alt="" />
+                <img src={star_dull_icon} alt="" />
+                <p>(122)</p>
+            </div>
+            <div className="productdisplay-right-prices">
+                <div className="productdisplay-right-prices-old">${product?.old_price}</div>
+                <div className="productdisplay-right-prices-new">${product?.new_price}</div>
+            </div>
+            <div className="productdisplay-right-description">
+                The Adidas Sporty Men Round Neck 3-Stripes Tee is built for men who value style, comfort, and high performance in their everyday activewear. Showcasing Adidas's signature 3-Stripes design, this lightweight sports t-shirt combines modern athletic aesthetics with moisture-wicking technology to keep you cool, dry, and comfortable during workouts, gym sessions, and outdoor training. Its breathable fabric enhances airflow, while the durable construction ensures long-lasting performance without losing shape or color. Perfect for sports and lifestyle needs, this versatile round neck tee is an all-season essential that seamlessly blends athletic function with everyday casual fashion.
+            </div>
+            <div className="productdisplay-right-size">
+                <h1>Select Size</h1>
+                <div className="productdisplay-right-sizes">
+                    <div>S</div>
+                    <div>M</div>
+                    <div>L</div>
+                    <div>XL</div>
+                    <div>XXL</div>
+                </div>
+            </div>
+            <button onClick={()=>{addToCart(product?.id)}}>ADD TO CART</button>
+            <p className='productdisplay-right-category'><span>Category: </span>Women, T-Shirt, Crop Top</p>
+            <p className='productdisplay-right-category'><span>Tags: </span>Modern, Latest </p>
+        </div>
+    </div>
+  )
+}
+
+export default ProductDisplay
